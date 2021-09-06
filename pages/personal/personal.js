adc721b8ae949a4631cb7eee23d4437270cfe41d
wangzheng <863676514@qq.com>
2021-09-06 22:34:59 +0800

    personal page add animation

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,11 +1,15 @@
 // pages/personal/personal.js
+let startY = 0 //起始位置
+let moveY = 0 //移动到的位置
+let distanceY = 0 //移动的距离
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    transformY: '',
+    trasitionY: ''
   },
 
   /**
@@ -14,7 +18,31 @@ Page({
   onLoad: function (options) {
 
   },
-
+  touchStart (event) {
+    startY = event.touches[0].clientY
+    this.setData({
+      trasitionY: ''
+    })
+  },
+  touchMove (event) {
+    moveY = event.touches[0].clientY
+    distanceY = moveY - startY
+    if(distanceY < 0){
+      return
+    }
+    if(distanceY >= 80){
+      distanceY = 80
+    }
+    this.setData({
+      transformY: `translateY(${distanceY}rpx)`
+    })
+  },
+  touchEnd (event) {
+    this.setData({
+      transformY: `translateY(0rpx)`,
+      trasitionY: `transform 1s linear`
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
